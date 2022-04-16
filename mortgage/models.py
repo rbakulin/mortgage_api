90063f3988b5e7e32c5ee1ef7c761a0a0d33e00c
rbakulin <roman190593@gmail.com>
2022-04-16 16:26:26 +0300

    Add model relations, add model to admin site

--- mortgage/models.py
+++ mortgage/models.py
@@ -10,18 +10,20 @@ class CreatedUpdatedModel(models.Model):
 
 
 class Mortgage(CreatedUpdatedModel):
-    percent = models.DecimalField(max_digits=2, decimal_places=2, verbose_name="Процент")
+    percent = models.DecimalField(max_digits=4, decimal_places=2, verbose_name="Процент")
     period = models.IntegerField(verbose_name="Срок кредита")
     first_payment_amount = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="ПВ")
     total_amount = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Сумма кредита")
     issue_date = models.DateField(null=True, blank=True, verbose_name="Дата выдачи")
+    real_estate_object = models.ForeignKey('RealEstateObject', unique=True, on_delete=models.CASCADE, null=True,
+                                           verbose_name='Объект недвижимости')
 
     class Meta:
         db_table = 'mortgage'
 
 
 class RealEstateObject(CreatedUpdatedModel):
-    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    price = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Цена")
     area = models.DecimalField(max_digits=5, decimal_places=1, verbose_name="Площадь")
     address = models.CharField(max_length=200, verbose_name="Адрес")
     built_year = models.IntegerField(verbose_name='Год постройки')
@@ -36,6 +38,7 @@ class Payment(CreatedUpdatedModel):
     bank_percent = models.DecimalField(max_digits=5, decimal_places=1, verbose_name="Процент банку")
     debt_decrease = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Уменьшение долга")
     debt_rest = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Остаток долга")
+    mortgage = models.ForeignKey('Mortgage', on_delete=models.CASCADE, null=True, verbose_name='Ипотечный кредит')
 
     class Meta:
         db_table = 'payment'
