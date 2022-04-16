ce281549ea3c6e47468846784a74972b92b3b4d6
rbakulin <roman190593@gmail.com>
2022-04-16 15:15:47 +0300

    Add re object and payment models

---
+++ mortgage/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class CreatedUpdatedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Mortgage(CreatedUpdatedModel):
+    percent = models.DecimalField(max_digits=2, decimal_places=2, verbose_name="Процент")
+    period = models.IntegerField(verbose_name="Срок кредита")
+    first_payment_amount = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="ПВ")
+    total_amount = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Сумма кредита")
+    issue_date = models.DateField(null=True, blank=True, verbose_name="Дата выдачи")
+
+    class Meta:
+        db_table = 'mortgage'
+
+
+class RealEstateObject(CreatedUpdatedModel):
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+    area = models.DecimalField(max_digits=5, decimal_places=1, verbose_name="Площадь")
+    address = models.CharField(max_length=200, verbose_name="Адрес")
+    built_year = models.IntegerField(verbose_name='Год постройки')
+
+    class Meta:
+        db_table = 'real_estate_object'
+
+
+class Payment(CreatedUpdatedModel):
+    date = models.DateField(null=True, blank=True, verbose_name="Дата")
+    amount = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Сумма")
+    bank_percent = models.DecimalField(max_digits=5, decimal_places=1, verbose_name="Процент банку")
+    debt_decrease = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Уменьшение долга")
+    debt_rest = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Остаток долга")
+
+    class Meta:
+        db_table = 'payment'
--- mortgage_api/settings.py
+++ mortgage_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    'credit'
+    'mortgage'
 ]
 
 MIDDLEWARE = [
