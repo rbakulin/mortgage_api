9182ec31d660f58e35c29f21e20ac053e7323326
rbakulin <roman190593@gmail.com>
2022-04-17 20:44:38 +0300

    Fix relation fields in serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,19 +2,23 @@ from rest_framework import serializers
 from mortgage.models import Mortgage, RealEstateObject, Payment
 
 
-class MortgageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mortgage
-        fields = ['id', 'percent', 'period', 'first_payment_amount', 'total_amount', 'issue_date', 'real_estate_object']
-
-
 class RealEstateObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = RealEstateObject
         fields = ['id', 'price', 'area', 'address', 'built_year']
 
 
+class MortgageSerializer(serializers.ModelSerializer):
+    real_estate_object = RealEstateObjectSerializer(many=False)
+
+    class Meta:
+        model = Mortgage
+        fields = ['id', 'percent', 'period', 'first_payment_amount', 'total_amount', 'issue_date']
+
+
 class PaymentSerializer(serializers.ModelSerializer):
+    mortgage = MortgageSerializer(many=False)
+
     class Meta:
         model = Payment
-        fields = ['id', 'date', 'amount', 'bank_percent', 'debt_decrease', 'debt_rest', 'mortgage']
+        fields = ['id', 'date', 'amount', 'bank_percent', 'debt_decrease', 'debt_rest']
