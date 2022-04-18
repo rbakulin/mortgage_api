53f6920af9f42c91cbc549b2f0b91b503cc21041
Roman Bakulin <roman190593@gmail.com>
2022-04-18 19:12:49 +0300

    Make payment parametrs dynamical

--- mortgage/admin.py
+++ mortgage/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Mortgage, RealEstateObject, Payment
 
-admin.site.register([Mortgage, RealEstateObject, Payment])
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['date', 'amount', 'mortgage', 'bank_percent', 'debt_decrease', 'debt_rest']
+    readonly_fields = ('bank_percent', 'debt_decrease', 'debt_rest')
+    ordering = ['date']
+
+
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register([Mortgage, RealEstateObject])
--- mortgage/models.py
+++ mortgage/models.py
@@ -35,10 +35,39 @@ class RealEstateObject(CreatedUpdatedModel):
 class Payment(CreatedUpdatedModel):
     date = models.DateField(null=True, blank=True, verbose_name="Дата")
     amount = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Сумма")
-    bank_percent = models.DecimalField(max_digits=5, decimal_places=1, verbose_name="Процент банку")
-    debt_decrease = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Уменьшение долга")
-    debt_rest = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="Остаток долга")
     mortgage = models.ForeignKey('Mortgage', on_delete=models.CASCADE, null=True, verbose_name='Ипотечный кредит')
+
+    def get_prev_payment(self):
+        past_payments = Payment.objects.filter(mortgage_id=self.mortgage_id, date__lt=self.date)
+        if past_payments:
+            return sorted(list(past_payments), key=lambda payment: payment.date, reverse=True)[0]
+        else:
+            return None
+
+    @property
+    def bank_percent(self):
+        prev_payment = self.get_prev_payment()
+        if prev_payment:
+            days_from_prev_payment = self.date - prev_payment.date
+            debt_rest = prev_payment.debt_rest
+        else:
+            days_from_prev_payment = self.date - self.mortgage.issue_date
+            debt_rest = self.mortgage.total_amount
+        bank_percent = (debt_rest * self.mortgage.percent * days_from_prev_payment.days) / (365 * 100)
+        return round(bank_percent, 2)
+
+    @property
+    def debt_decrease(self):
+        return self.amount - self.bank_percent
+
+    @property
+    def debt_rest(self):
+        prev_payment = self.get_prev_payment()
+        if prev_payment:
+            prev_amount = prev_payment.debt_rest
+        else:
+            prev_amount = self.mortgage.total_amount
+        return prev_amount - self.debt_decrease
 
     class Meta:
         db_table = 'payment'
