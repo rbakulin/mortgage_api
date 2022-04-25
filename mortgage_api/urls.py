2458edc7575c7db9097a6d64a303c0aebd427c89
Roman Bakulin <roman190593@gmail.com>
2022-04-25 23:42:32 +0300

    API shouldn't be a separate module

--- mortgage_api/urls.py
+++ mortgage_api/urls.py
@@ -19,6 +19,6 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('mortgage.api.urls')),
     path('api-token-auth/', views.obtain_auth_token),
-    path('api/', include('api.urls'))
 ]
