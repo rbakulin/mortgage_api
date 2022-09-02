b0262255835a2c418bb3c956d8a34fb9bdfe2bb6
Roman Bakulin <roman190593@gmail.com>
2022-09-02 15:23:06 +0300

    Add all extra payment logic

--- mortgage/models.py
+++ mortgage/models.py
@@ -29,27 +29,34 @@ class Payment(CreatedUpdatedModel):
     mortgage = models.ForeignKey('Mortgage', on_delete=models.CASCADE, null=True, related_name="payments",
                                  verbose_name='mortgage')
     is_extra = models.BooleanField(verbose_name='is extra payment', default=False)
-    # TODO: rename to bank_sum?
+    # TODO: rename to bank_amount?
     bank_share = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="bank share", null=True)
     debt_decrease = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="debt decrease", null=True)
     debt_rest = models.DecimalField(max_digits=11, decimal_places=2, verbose_name="debt rest", null=True)
 
     def get_prev_payment(self):
-        past_payments = Payment.objects.filter(mortgage_id=self.mortgage_id, date__lte=self.date)
+        past_payments = Payment.objects.filter(mortgage_id=self.mortgage_id, date__lte=self.date).exclude(pk=self.pk)
         if past_payments:
             return sorted(list(past_payments), key=lambda payment: payment.date, reverse=True)[0]
+        else:
+            return None
+
+    def get_next_payment(self):
+        next_payments = Payment.objects.filter(mortgage_id=self.mortgage_id, date__gte=self.date).exclude(pk=self.pk)
+        if next_payments:
+            return sorted(list(next_payments), key=lambda payment: payment.date)[0]
         else:
             return None
 
     def calc_bank_share(self):
         prev_payment = self.get_prev_payment()
         if prev_payment:
-            days_from_prev_payment = self.date - prev_payment.date
+            time_from_prev_payment = self.date - prev_payment.date
             debt_rest = prev_payment.debt_rest
         else:
-            days_from_prev_payment = self.date - self.mortgage.issue_date
+            time_from_prev_payment = self.date - self.mortgage.issue_date
             debt_rest = self.mortgage.total_amount
-        bank_percent = (debt_rest * self.mortgage.percent * days_from_prev_payment.days) / (365 * 100)
+        bank_percent = (debt_rest * self.mortgage.percent * time_from_prev_payment.days) / (365 * 100)
         return round(bank_percent, 2)
 
     def calc_debt_decrease(self):
