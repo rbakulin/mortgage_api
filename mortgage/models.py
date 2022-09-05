5c23d2d3a48a6263677ae33145e5775ca0be24f6
Roman Bakulin <roman190593@gmail.com>
2022-09-05 21:11:35 +0300

    DRY api view, make culc schedule a POST

--- mortgage/models.py
+++ mortgage/models.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 
 from django.db import models
+from rest_framework import status
+from rest_framework.response import Response
 
 from .helpers import days_in_year, get_timedelta, get_last_day_in_months
 
@@ -33,6 +35,15 @@ class Mortgage(CreatedUpdatedModel):
     @property
     def monthly_percent(self):
         return Decimal(self.percent / (12 * 100))  # 1/12 of credit's percent in 0.xx format
+
+    @staticmethod
+    def git_mortgage(mortgage_id):
+        try:
+            current_mortgage = Mortgage.objects.get(pk=mortgage_id)
+        except Mortgage.DoesNotExist:
+            return Response(data={'error': f'Mortgage with id {mortgage_id} does not exist'},
+                            status=status.HTTP_404_NOT_FOUND)
+        return current_mortgage
 
     class Meta:
         db_table = 'mortgage'
