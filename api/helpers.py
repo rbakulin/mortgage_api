851400dbe7e3dc3ebbffa87a14f6162ac799ebe7
Roman Bakulin <roman190593@gmail.com>
2023-08-25 22:40:44 +0400

    Refactor put and patch methods of mortgage

--- api/helpers.py
+++ api/helpers.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse
+from typing import Callable
+
+from django.http import HttpRequest, HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 
 from mortgage.messages import responses
-from mortgage.models import Mortgage
+from mortgage.models import Mortgage, Payment
+from mortgage.payment_schedule import PaymentScheduler
 
 
-def check_mortgage_permissions(mortgage_id: int, user_id: int) -> HttpResponse:
+def check_mortgage_permissions(
+        mortgage_id: int, user_id: int, http_method: Callable, request: HttpRequest, *args, **kwargs) -> HttpResponse:
     current_mortgage = Mortgage.get_mortgage(mortgage_id)
     if not current_mortgage:
         return Response(data={'detail': responses.MORTGAGE_NOT_FOUND},
@@ -14,4 +18,13 @@ def check_mortgage_permissions(mortgage_id: int, user_id: int) -> HttpResponse:
     if current_mortgage.user.pk != user_id:
         return Response(data={'detail': responses.MORTGAGE_NOT_BELONG},
                         status=status.HTTP_403_FORBIDDEN)
-    return Response(status=status.HTTP_200_OK)
+    return http_method(request, *args, **kwargs)
+
+
+def update_payment_schedule(mortgage_id: int) -> None:
+    current_mortgage = Mortgage.get_mortgage(mortgage_id)
+    current_payments = Payment.objects.filter(mortgage_id=current_mortgage.pk)
+    if current_payments:
+        current_payments.delete()  # delete old payment schedule if it exists
+        payment_scheduler = PaymentScheduler(mortgage=current_mortgage)
+        payment_scheduler.calc_and_save_payments_schedule()
