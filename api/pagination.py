80691effaa90745c540c264a4e7b8e37174cd542
Roman Bakulin <roman190593@gmail.com>
2023-08-25 16:38:15 +0300

    Remove redundant pagination

--- api/pagination.py
+++ api/pagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPagination(PageNumberPagination):
-    page_size = 10
+class PaymentPagination(PageNumberPagination):
+    page_size = 500
     page_size_query_param = 'page_size'
