9f1155e9ed7d0df1a36fe4de6f247157d72daaa2
Roman Bakulin <roman190593@gmail.com>
2023-08-27 17:34:26 +0300

    Add filter to payment and mortgage

--- mortgage/admin.py
+++ mortgage/admin.py
@@ -4,12 +4,14 @@ from .models import Mortgage, Payment
 
 
 class PaymentAdmin(admin.ModelAdmin):
+    list_filter = ['mortgage__id']
     list_display = ['date', 'amount', 'mortgage_id', 'bank_amount', 'debt_decrease', 'debt_rest', 'is_extra']
     readonly_fields = ('bank_amount', 'debt_decrease', 'debt_rest')
     ordering = ['mortgage', 'date', 'created_at']
 
 
 class MortgageAdmin(admin.ModelAdmin):
+    list_filter = ['user']
     list_display = ['pk', 'percent', 'period', 'first_payment_amount', 'apartment_price', 'total_amount', 'issue_date',
                     'user']
 
